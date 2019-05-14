7fc12bceef6f8c053d91ef47be06860a7033de62
xpl <rocket.mind@gmail.com>
2019-05-14 17:08:23 +0300

    added webpack demo, fixed webpack-related sourcemapping issues in the deps

---
+++ demo/webpack-vanilla/index.js
@@ -0,0 +1,13 @@
+/*
+
+    Run with this demo with "npm run demo-parcel-vanilla" (in the repo root directory).
+    
+*/
+
+import panic from '../../panic-overlay'
+
+function helloWorld () {
+    document.body.appendChild (666)
+}
+
+helloWorld ()
